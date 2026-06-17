7e2980077883b2b9ccb7af2ae7edbdf8b1fcbc4b
unknown <jadebrosas143@gmail.com>
2026-06-17 14:21:14 +0800

    Initial frontend commit

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const api = axios.create({
 
-    baseURL:'http://127.0.0.1:8000/api',
+ baseURL: 'https://govtrack-api-xxxx.onrender.com/api',
 
     headers:{
         Accept:'application/json'
