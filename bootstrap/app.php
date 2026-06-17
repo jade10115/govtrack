d6fabd2f55ef798c8c5b6ef083b6e3d66262be46
unknown <jadebrosas143@gmail.com>
2026-06-17 17:45:27 +0800

    new commit

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,18 +6,19 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-    web: __DIR__.'/../routes/web.php',
-    api: __DIR__.'/../routes/api.php',
-    commands: __DIR__.'/../routes/console.php',
-    health: '/up',
-)
-    ->withMiddleware(function (Middleware $middleware): void {
-
-    $middleware->alias([
-        'role' => \App\Http\Middleware\RoleMiddleware::class,
-    ]);
-        //
+        web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
+        commands: __DIR__.'/../routes/console.php',
+        health: '/up',
+    )
+    ->withMiddleware(function (Middleware $middleware) {
+        // Fix CORS right here for Laravel 11
+        $middleware->statefulApi();
+        
+        $middleware->validateCsrfTokens(except: [
+            'api/*', // Bypasses CSRF tokens for all API routes since we use Bearer Tokens
+        ]);
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
+    ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
