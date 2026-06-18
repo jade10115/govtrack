8e80031b8f9b1f3b7a42885b9d5847198008ff35
unknown <jadebrosas143@gmail.com>
2026-06-18 15:36:31 +0800

    Security: Remove temporary setup route from production

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Artisan;
 
 Route::get('/', function () {
     return view('welcome');
-});
-
-// The Secret Database Builder Route
-Route::get('/setup-database', function () {
-    try {
-        // We changed this to db:seed to create your admin account!
-        Artisan::call('db:seed', ['--force' => true]);
-        return 'SUCCESS: Database seeded perfectly! Admin account created.';
-    } catch (\Exception $e) {
-        return 'ERROR: ' . $e->getMessage();
-    }
 });
