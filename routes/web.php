70196da1576381c941cabb4adeddd2ed9997f60d
unknown <jadebrosas143@gmail.com>
2026-06-18 15:23:01 +0800

    Add temporary route to migrate database

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/', function () {
-    return response()->json([
-        'message' => 'Laravel is working'
-    ]);
+// The Secret Database Builder Route
+Route::get('/setup-database', function () {
+    try {
+        Artisan::call('migrate', ['--force' => true]);
+        return 'SUCCESS: Database tables created perfectly!';
+    } catch (\Exception $e) {
+        return 'ERROR: ' . $e->getMessage();
+    }
 });
